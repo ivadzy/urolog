af2870280f39eaaa551d45d126a0b74934660836
Ivan <ivadzy@gmail.com>
2019-08-29 16:15:21 +0300

    Add LogController, add basic creation test

---
+++ Urolog/LogController.swift
@@ -0,0 +1,14 @@
+//
+//  LoggerController.swift
+//  Urolog
+//
+//  Created by Ivan on 8/29/19.
+//  Copyright © 2019 ivadzy. All rights reserved.
+//
+
+import Foundation
+
+class LogController
+{
+    
+}
--- UrologTests/UrologTests.swift
+++ UrologTests/UrologTests.swift
@@ -9,26 +9,16 @@
 import XCTest
 @testable import Urolog
 
-class UrologTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class UrologTests: XCTestCase
+{
+    func test_logControllerInstanceCreation_resultsNotNil()
+    {
+        let result = LogController()
+        XCTAssertNotNil(result)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_logController_printsMessagesToConsole()
+    {
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
