d5564b854f2206ed0d502e5cda351bd025fbe8e6
Ivan <ivadzy@gmail.com>
2019-08-29 14:57:03 +0300

    Initial Commit

---
+++ Urolog/Urolog.h
@@ -0,0 +1,19 @@
+//
+//  Urolog.h
+//  Urolog
+//
+//  Created by Ivan on 8/29/19.
+//  Copyright © 2019 ivadzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Urolog.
+FOUNDATION_EXPORT double UrologVersionNumber;
+
+//! Project version string for Urolog.
+FOUNDATION_EXPORT const unsigned char UrologVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Urolog/PublicHeader.h>
+
+
